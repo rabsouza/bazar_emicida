10ba48e3c53f93a43f5874a4153e9e02807daec9
Rafael Alessandro Batista de Souza <rabsouza@gmail.com>
2026-06-15 21:34:06 -0300

    fix: remove a categoria errada de jogos de tabuleiro

--- js/config.js
+++ js/config.js
@@ -8,7 +8,6 @@ const CONFIG = {
   CATEGORIAS: [
     { id: 'motos',          nome: 'Moto',               icone: '🏍️', descricao: 'Peças, acessórios e equipamentos para motociclistas' },
     { id: 'eletronicos',    nome: 'Eletrônicos',         icone: '📱', descricao: 'Gadgets, cabos, equipamentos e tudo que tem chip' },
-    { id: 'jogos-tabuleiro',nome: 'Jogos de Tabuleiro',  icone: '🎲', descricao: 'Estratégia, diversão e muito blefe na mesa' },
     { id: 'boardgames',     nome: 'Jogos de Tabuleiro',  icone: '🎲', descricao: 'Estratégia, diversão e muito blefe na mesa' },
     { id: 'moveis',         nome: 'Móveis',              icone: '🪑', descricao: 'Móveis bem conservados prontos pra um novo lar' },
     { id: 'casa',           nome: 'Casa',                icone: '🏠', descricao: 'Itens do dia a dia pra deixar o lar redondo' },
